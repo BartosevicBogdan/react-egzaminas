97a04da52b2084475cad3f09b202fb3712740a57
BartosevicBogdan <95608837+BartosevicBogdan@users.noreply.github.com>
2022-04-14 12:12:44 +0300

    added: BrowserRouter, Routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import "./App.css";
+import "./App.scss";
+import AddPage from "./pages/AddPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import HomePage from "./pages/HomePage";
+import Header from "./components/Header";
+import { Route } from "react-router-dom";
+import { Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <h1>Test</h1>
+      <Header />
+      <Switch>
+        <Route path={"/"} exact>
+          <HomePage />
+        </Route>
+        <Route path={"/add"}>
+          <AddPage />
+        </Route>
+        <Route path={"/login"}>
+          <LoginPage />
+        </Route>
+        <Route path={"/register"}>
+          <RegisterPage />
+        </Route>
+      </Switch>
     </div>
   );
 }
